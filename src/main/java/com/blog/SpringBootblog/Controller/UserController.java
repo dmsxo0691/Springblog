f897543b731df8295d0a665e7eb94b57e8443462
dmsxo0691 <110580287+dmsxo0691@users.noreply.github.com>
2022-12-09 15:10:08 +0900

    오류 수정

--- src/main/java/com/blog/SpringBootblog/Controller/UserController.java
+++ src/main/java/com/blog/SpringBootblog/Controller/UserController.java
@@ -1,7 +1,9 @@
 package com.blog.SpringBootblog.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class UserController {
 
     @GetMapping("/user/joinForm")
